8c690910ecee45ed605d0e42d30623f8d4615804
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-03 15:51:52 -0300

    Adicionando tratamento de excessões

--- ToDoOrder/Program.cs
+++ ToDoOrder/Program.cs
@@ -9,41 +9,52 @@ namespace ToDoOrder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter client data:");
-            Console.Write("Name: ");
-            string name = Console.ReadLine();
-            Console.Write("Email: ");
-            string email = Console.ReadLine();
-            Console.Write("Birth date (DD/MM/YYYY): ");
-            DateTime birthDate = DateTime.Parse(Console.ReadLine());
-            Console.Write("Status (PENDINNG_PAYMENT,PROCESSING,SHIIPPED,DELIVERED): ");
-            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine().ToUpper());
-
-            Client client = new Client(name, email, birthDate);
-            Order order = new Order(DateTime.Now, status, client);
-
-            Console.Write("How many items to this order? ");
-            int qtdeItems = int.Parse(Console.ReadLine());
-
-            for (int i = 1; i <= qtdeItems; i++)
+            try
             {
-                Console.WriteLine($"Enter #{i} item data:");
-                Console.Write("Product name: ");
-                string nameProduct = Console.ReadLine();
-                Console.Write("Product price: ");
-                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                Console.Write("Quantity: ");
-                int qtde = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter client data:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Birth date (DD/MM/YYYY): ");
+                DateTime birthDate = DateTime.Parse(Console.ReadLine());
+                Console.Write("Status (PENDINNG_PAYMENT,PROCESSING,SHIIPPED,DELIVERED): ");
+                OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine().ToUpper());
 
-                Product product = new Product(nameProduct, price);
-                OrderItem item = new OrderItem(qtde, price, product);
+                Client client = new Client(name, email, birthDate);
+                Order order = new Order(DateTime.Now, status, client);
 
-                order.AddItem(item);
+                Console.Write("How many items to this order? ");
+                int qtdeItems = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= qtdeItems; i++)
+                {
+                    Console.WriteLine($"Enter #{i} item data:");
+                    Console.Write("Product name: ");
+                    string nameProduct = Console.ReadLine();
+                    Console.Write("Product price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Quantity: ");
+                    int qtde = int.Parse(Console.ReadLine());
+
+                    Product product = new Product(nameProduct, price);
+                    OrderItem item = new OrderItem(qtde, price, product);
+
+                    order.AddItem(item);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("ORDER SUMARY:");
+                Console.WriteLine(order);
             }
-
-            Console.WriteLine();
-            Console.WriteLine("ORDER SUMARY:");
-            Console.WriteLine(order);
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Erro de formatação: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro inesperado: {e.Message}");
+            }
         }
     }
 }
